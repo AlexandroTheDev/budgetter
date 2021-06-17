f54a8f9a65a65488e042cf2ef45ef49642035ea0
Alexandro Mena <alexandrothedev@gmail.com>
2021-06-17 22:09:03 +0800

    fix budget submmary

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,34 @@
-import { Container, Row, Col, Card } from "react-bootstrap"
+import moment from "moment"
+import { useEffect } from "react"
+import { useContext, useState } from "react"
+import { Container, Row, Col, Card, Spinner } from "react-bootstrap"
+import { RecordContext } from "../../contexts/record/RecordContext"
 import numberPriceFormat from "../../utils/numberPriceFormat"
 
 export default function Home() {
-    let currentMonthTotalIncome = 12354.988
-    let currentMonthTotalExpenses = 4523.00
+    const { records, isLoading, getRecords } = useContext(RecordContext)
+
+    const [summary, setSummary] = useState({
+        currentMonthTotalIncome: 0,
+        currentMonthTotalExpenses :0
+    })
+
+    useEffect(() => {
+        setSummary({
+            currentMonthTotalIncome : records.filter( record => record.type === 'income' && moment(new Date()).format('MM') === moment(record.date).format('MM')).reduce( (x,y) => x + y.amount, 0 ),
+            currentMonthTotalExpenses : records.filter( record => record.type === 'expense' && moment(new Date()).format('MM') === moment(record.date).format('MM')).reduce( (x,y) => x + y.amount, 0 )
+        })
+    },[records])
+
+    useEffect(() => {
+        console.log("test")
+        getRecords()
+    }, [])
+
     return (
+        isLoading ?
+        <><Spinner animation="border"></Spinner> Loading </>
+        :
         <Container>
             <Row>
                 <Col xs="12" sm="6" md="4" className="mx-auto mb-3">
@@ -12,7 +36,7 @@ export default function Home() {
                         <Card.Body>
                             <div className="d-flex">
                                 <div>
-                                    <p>&#8369; {numberPriceFormat(currentMonthTotalIncome)}</p>
+                                    <p>&#8369; {numberPriceFormat(summary.currentMonthTotalIncome)}</p>
                                     <small>This Month Income</small>
                                 </div>
                                 <div className="ml-auto align-self-center">
@@ -30,7 +54,7 @@ export default function Home() {
                         <Card.Body>
                             <div className="d-flex">
                                 <div>
-                                    <p>&#8369; {numberPriceFormat(currentMonthTotalExpenses)}</p>
+                                    <p>&#8369; {numberPriceFormat(summary.currentMonthTotalExpenses)}</p>
                                     <small>This Month Expenses</small>
                                 </div>
                                 <div className="ml-auto align-self-center">
@@ -47,7 +71,7 @@ export default function Home() {
                         <Card.Body>
                                 <div className="d-flex">
                                     <div>
-                                        <p>&#8369; {numberPriceFormat(currentMonthTotalIncome - currentMonthTotalExpenses)}</p>
+                                        <p>&#8369; {numberPriceFormat(summary.currentMonthTotalIncome - summary.currentMonthTotalExpenses)}</p>
                                         <small>Budget Left</small>
                                     </div>
                                     <div className="ml-auto align-self-center">
